43616347b5ac3867bb5f0dd502c6055d781a9038
brandonalcantara17 <brandonalcantara@proton.me>
2026-04-08 16:03:06 +0200

    Updated migration for users table and added seeder for users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin',
+                'email' => 'admin@farmaciasoler.com',
+                'password' => Hash::make('password'),                
+                'isAdmin' => 1,
+                'isActive' => 1,
+                'isPrivate' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+                'email_verified_at' => now(),
+            ],
+        ]);
+    }
+}
