899ed558b65c3247acaa999b9d3577ae1a5f1f9c
brandonalcantara17 <brandonalcantara@proton.me>
2026-04-08 18:16:32 +0200

    Created home page and navbar and webapp leyout

---
+++ resources/js/layouts/WebAppLayout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue';
+</script>
+
+<template>
+    <Navbar/>
+    <div class="min-h-[80dvh] pt-20">
+        <slot />
+    </div>
+    
+</template>
