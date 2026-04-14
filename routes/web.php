19a9b40a3fe734988283f35c309bbf05b9620425
Charfah ilyass <charfahilyass@gmail.com>
2026-04-15 01:39:17 +0200

    Feature/get_Servei_from_database

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomePageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ServiceController;
 use Laravel\Fortify\Features;
 use Inertia\Inertia;
 
@@ -11,10 +12,8 @@ Route::inertia('/', 'Welcome', [
     'canRegister' => Features::enabled(Features::registration()),
 ])->name('home');
 //pedir cita 
-Route::get('/pedir-cita', function () {
-    return Inertia::render('PedirCita');
-})->name('pedir-cita');
-
+//Route::get('/pedir-cita', function () { return Inertia::render('PedirCita');})->name('pedir-cita');
+Route::get('/pedir-cita', [ServiceController::class, 'index'])->name('pedir-cita');;
 // Private Routes
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
