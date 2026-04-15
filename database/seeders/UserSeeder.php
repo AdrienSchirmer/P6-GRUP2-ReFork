3cdd1186df5375a0bb1fbc6aa96c85b45740a01b
brandonalcantara17 <brandonalcantara@proton.me>
2026-04-15 15:36:56 +0200

    Fixed assignement creation validation so it matches users table columns fields

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,8 +23,6 @@ class UserSeeder extends Seeder
                 'created_at' => now(),
                 'updated_at' => now(),
                 'email_verified_at' => now(),
-
-
             ],
         ]);
     }
