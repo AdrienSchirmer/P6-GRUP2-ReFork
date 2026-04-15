66238178ea07f0f3e9cac2c408ec3c83097ccee2
brandonalcantara17 <brandonalcantara@proton.me>
2026-04-15 17:00:45 +0200

    Only logged users can send an assignment

--- app/Http/Requests/CreateAssignmentRequest.php
+++ app/Http/Requests/CreateAssignmentRequest.php
@@ -12,7 +12,7 @@ class CreateAssignmentRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true; // For anyone even without an account
+        return false; // For anyone with an account
     }
 
     /**
