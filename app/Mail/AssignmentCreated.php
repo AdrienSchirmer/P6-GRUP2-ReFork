09ddd3de210d87b5da27acad261150fd15162e3f
brandonalcantara17 <brandonalcantara@proton.me>
2026-04-20 16:38:29 +0200

    Email now sends correctly

--- app/Mail/AssignmentCreated.php
+++ app/Mail/AssignmentCreated.php
@@ -38,7 +38,7 @@ class AssignmentCreated extends Mailable
     public function content(): Content
     {
         return new Content(
-            text: 'assignment-created',
+            view: 'mail.assignment-created',
         );
     }
 
