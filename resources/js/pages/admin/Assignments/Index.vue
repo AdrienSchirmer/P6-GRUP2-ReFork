1064b1c2f7e9a20cd014d2ac5ede5136afb84467
brandonalcantara17 <brandonalcantara@proton.me>
2026-04-22 17:04:19 +0200

    Admin panel assignments is now responsive

--- resources/js/pages/admin/Assignments/Index.vue
+++ resources/js/pages/admin/Assignments/Index.vue
@@ -21,7 +21,7 @@ import { format } from 'timeago.js';
 </script>
 <template>
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="grid grid-cols-3 gap-5 pr-10">
+        <div class="grid grid-cols-1 gap-5 pr-10 md:grid-cols-2 lg:grid-cols-3">
             <div
                 v-for="ass in assignments"
                 :key="ass.name"
